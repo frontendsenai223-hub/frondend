46a68c54413022a4f2ad6b3fefeaa777363f1c0e
frontendsenai223-hub <frontendsenai223@gmail.com>
2025-08-05 16:50:13 -0300

    Add files via upload

---
+++ landing-page/a.js
@@ -0,0 +1,32 @@
+<section class="frases">
+            <h2>o que dizem nossos alunos</h2>
+            <div class="cards">
+                <div class="card">
+                    <P>"Aprender frontend ficou muito mais fácil com esse projeto. Agora consigo criar minhas próprias
+                        páginas!"</P>
+                    <h3>João Pereira</h3>
+                </div>
+                <div class="card">
+                    <P>"As explicações são diretas e os exemplos me ajudaram a entender o que eu achava impossível."</P>
+                    <h3>Maria Oliveira</h3>
+                </div>
+                <div class="card">
+                    <P>"O projeto me deu confiança para montar meu portfólio e participar de processos seletivos."</P>
+                    <h3>Ana Costa</h3>
+                </div>
+            </div>
+        </section>
+
+
+.frases {
+    padding: 4rem 2rem;
+    background-color: #fff;
+    text-align: center;
+
+}
+
+.frases h2 {
+    font-size: 2rem;
+    color: #111827;
+    margin-bottom: 2rem;
+}
